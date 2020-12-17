77d9d8c92e056443bf10e6be1f9d1e7cf0e68047
Markus Pettersson <markus05.pettersson@gmail.com>
2020-12-17 17:22:18 +0100

    TestHarness is overrated

---
+++ Assets/ModuleScripts/Bag.cs
@@ -0,0 +1,28 @@
+﻿public class Bag
+{
+    public BagType Type { get; set; }
+
+    public BagColor Color { get; set; }
+
+    public int Value { get; set; }
+
+    public string Label { get; set; }
+
+    public bool IsReadOnly {get; set; }
+
+    public int Index { get; set; }
+}
+
+public enum BagType
+{
+    Money,
+    Diamond
+}
+
+public enum BagColor
+{
+    Normal,
+    Red,
+    Blue
+}
+
